7292fac0eed56214e3e8812dd610d589f000ab1e
Geison Amorim <geisonam43@gmail.com>
2022-07-15 14:46:17 -0300

    Primeiros Componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButtom"
 
 function App() {
   return(
+    <>
     <h1>Olá mundo</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 }
 
