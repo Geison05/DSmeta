d9747363ea51591d05d31524b9d71015b0bb1d53
Geison Amorim <geisonam43@gmail.com>
2022-07-15 16:22:54 -0300

    Outros Componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import NotificationButton from "./components/NotificationButtom"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
-  return(
+  return (
     <>
-    <h1>Olá mundo</h1>
-    <NotificationButton/>
-    <NotificationButton/>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-conteiner">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
